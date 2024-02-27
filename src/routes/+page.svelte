fc8301a50a8cfab630a3c6ef1606432c8796df43
0302BaStI <quinesser.sebastian@gmail.com>
2024-02-27 14:21:50 +0100

    k

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <script>
-    import {supabase} from '$lib/supabase';
- 
-    let promise = supabase.from('countries').select();
+	import { supabase } from '$lib/supasbase';
+
+	let promise = supabase.from('countries').select();
 </script>
- 
+
 {#await promise}
-    <div>Loading...</div>
+	<div>Loading...</div>
 {:then result}
-    <div>{JSON.stringify(result)}</div>
+	<div>{JSON.stringify(result)}</div>
 {/await}
