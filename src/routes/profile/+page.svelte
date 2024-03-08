c6ec369d71d3635b8e3074e6f158742c974245b8
0302BaStI <quinesser.sebastian@gmail.com>
2024-03-08 09:03:52 +0100

    Update +page.svelte

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -4,8 +4,6 @@
 	import { supabase } from '$lib/supabase';
 	import { session } from '$lib/store';
 
-	export let session: AuthSession;
-
 	let loading = false;
 	let username: string | null = null;
 	let website: string | null = null;
@@ -45,7 +43,7 @@
 	async function updateProfile() {
 		try {
 			loading = true;
-			const { user } = session;
+			const { user } = $session;
 
 			const updates = {
 				id: user.id,
@@ -69,23 +67,27 @@
 		}
 	}
 </script>
+
 {#if $session}
-<form on:submit|preventDefault={updateProfile} class="form-widget">
-	<div>Email: {session.user.email}</div>
-	<div>
-		<label for="username">Name</label>
-		<input id="username" type="text" bind:value={username} />
-	</div>
-	<div>
-		<label for="website">Website</label>
-		<input id="website" type="text" bind:value={website} />
-	</div>
-	<div>
-		<button type="submit" class="button primary block" disabled={loading}>
-			{loading ? 'Saving ...' : 'Update profile'}
+	<form on:submit|preventDefault={updateProfile} class="form-widget">
+		<div>Email: {$session.user.email}</div>
+		<div>
+			<label for="username">Name</label>
+			<input id="username" type="text" bind:value={username} />
+		</div>
+		<div>
+			<label for="website">Website</label>
+			<input id="website" type="text" bind:value={website} />
+		</div>
+		<div>
+			<button type="submit" class="button primary block" disabled={loading}>
+				{loading ? 'Saving ...' : 'Update profile'}
+			</button>
+		</div>
+		<button type="button" class="button block" on:click={() => supabase.auth.signOut()}>
+			Sign Out
 		</button>
-	</div>
-	<button type="button" class="button block" on:click={() => supabase.auth.signOut()}>
-		Sign Out
-	</button>
-</form>
+	</form>
+{:else}
+	<div>seems you're not logged in!</div>
+{/if}
