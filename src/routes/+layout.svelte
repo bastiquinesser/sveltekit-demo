7b60a96f7af20d53e2fd0243f5412c75488c3b60
0302BaStI <quinesser.sebastian@gmail.com>
2024-03-08 09:08:12 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,11 @@
 </script>
 
 <div data-theme={$theme} class="min-h-screen p-3">
-	<nav class="flex items-center gap-8 m-4" />
+	<nav class="flex items-center gap-8 m-4">
+		<a href="/" class="text-center">Home</a>|
+		<a href="/login" class="text-center">Login</a>|
+		<a href="/profile" class="text-center">Profile</a>
+	</nav>
 
 	<select bind:value={$theme} class="fixed top-0 right-0">
 		<option value="cyberpunk">cyberpunk</option>
