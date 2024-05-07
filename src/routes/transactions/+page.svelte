7108d00a88562a215fc1b518370ead432c79568b
0302BaStI <quinesser.sebastian@gmail.com>
2024-05-07 14:27:38 +0200

    k

---
+++ src/routes/transactions/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { supabase } from '$lib/supabase';
+	import { onMount } from 'svelte';
+
+	let transactionList = [':)'];
+
+	async function getTransactions() {
+		let { data: transactions, error } = await supabase.from('transaction').select('*');
+		transactionList = transactions;
+	}
+
+	onMount(() => {
+		getTransactions();
+	});
+</script>
+
+<div>{JSON.stringify(transactionList)}</div>
